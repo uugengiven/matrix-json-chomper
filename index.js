9585847f4cb8f330e420ab5939a0cf3621489bc5
John Lange <lange.john.m@gmail.com>
2023-12-13 16:01:13 -0500

    Update to read input folder and allow for input filename or input filename + roomname

--- index.js
+++ index.js
@@ -2,216 +2,245 @@
 
 const fs = require('fs');
 
+const start = () => {
+    // get command line arguments
+    const args = process.argv.slice(2);
+    const filename = args[0];
+    const roomName = args[1];
 
-let rawdata = fs.readFileSync('export.json');
-let info = JSON.parse(rawdata);
-
-const team = "default"
-const room = info.room_name.replaceAll(" ", "-").toLowerCase();
-
-
-let users = {}; // hash table of all users
-let posts = [];
-let channel = JSON.stringify({
-    type: "channel",
-    channel: {
-        team,
-        name: room,
-        purpose: info.topic,
-        type: "O"
-    }
-});
-
-
-const getFileTime = (timestamp) => {
-    const ts = new Date(timestamp);
-    let hours = ts.getHours();
-    let ampm = "AM"
-    if(hours > 12)
+    if(filename) 
     {
-        hours = hours - 12;
-        ampm = "PM"
+        generateOutput(filename, roomName);
     }
-    if(hours === 0)
+    else
     {
-        hours = 12;
+        // get a list of all *.json files in the folder
+        const allFiles = fs.readdirSync('./input');
+        const jsonFiles = allFiles.filter(file => file.endsWith('.json'));
+
+        jsonFiles.forEach(file => {
+            generateOutput(file);
+        });
+
     }
-    const minutes = ts.getMinutes().toString().padStart(2, '0');
-    const seconds = ts.getSeconds().toString().padStart(2, '0');
-
-    const suffix = `-${ts.getMonth()+1}-${ts.getDate()}-${ts.getFullYear()} at ${hours}-${minutes}-${seconds} ${ampm}`
-
-    console.log(suffix);
-    return suffix;
 }
 
-const injectFileTime = (filename, timestamp) => {
-    const filetime = getFileTime(timestamp);
 
-    const index = filename.lastIndexOf(".");
-    const filetype = filename.slice(index);
-    const name = filename.slice(0, index);
 
-    return name + filetime + filetype;
-}
+const generateOutput = (fileName, roomName) => {
+    let rawdata = fs.readFileSync(`./input/${fileName}`);
+    let info = JSON.parse(rawdata);
 
-const messageToPost = (message) => {
-    const user = message.user_id;
-    if(!users[user])
-    {
-        users[user] = user.slice(1, user.length - 23) + "_matrix";
-    }
+    const team = "default"
+    const room = roomName ? roomName.replaceAll(" ", "-").toLowerCase() : info.room_name.replaceAll(" ", "-").toLowerCase();
 
-    const post = {
-        type: "post",
-        post: {
+
+    let users = {}; // hash table of all users
+    let posts = [];
+    let channel = JSON.stringify({
+        type: "channel",
+        channel: {
             team,
-            channel: room,
-            user: users[user],
-            message: message.content.body,
-            create_at: message.origin_server_ts
+            name: room,
+            purpose: info.topic,
+            type: "O"
         }
-    }
-
-    return post;
-}
-
-const iamgeToPost = (message) => {
-    const user = message.user_id;
-    if(!users[user])
-    {
-        users[user] = user.slice(1, user.length - 23) + "_matrix";
-    }
-
-    const filename = injectFileTime(message.content.body, message.origin_server_ts);
-
-    console.log(filename)
-    const post = {
-        type: "post",
-        post: {
-            team,
-            channel: room,
-            user: users[user],
-            message: message.content.body,
-            create_at: message.origin_server_ts,
-            attachments: [{
-                path: "/tmp/import/images/" + filename
-            }]
-        }
-    }
+    });
 
 
-
-    return post;
-}
-
-const fileToPost = (message) => {
-    const user = message.user_id;
-    if(!users[user])
-    {
-        users[user] = user.slice(1, user.length - 23) + "_matrix";
-    }
-
-    const filename = injectFileTime(message.content.body, message.origin_server_ts);
-    console.log(filename)
-    const post = {
-        type: "post",
-        post: {
-            team,
-            channel: room,
-            user: users[user],
-            message: message.content.body,
-            create_at: message.origin_server_ts,
-            attachments: [{
-                path: "/tmp/import/files/" + filename
-            }]
-        }
-    }
-
-    return post;
-}
-
-const videoToPost = (message) => {
-    const user = message.user_id;
-    if(!users[user])
-    {
-        users[user] = user.slice(1, user.length - 23) + "_matrix";
-    }
-
-    const filename = injectFileTime(message.content.body, message.origin_server_ts);
-    console.log(filename)
-    const post = {
-        type: "post",
-        post: {
-            team,
-            channel: room,
-            user: users[user],
-            message: message.content.body,
-            create_at: message.origin_server_ts,
-            attachments: [{
-                path: "/tmp/import/videos/" + filename
-            }]
-        }
-    }
-
-    return post;
-}
-
-
-
-info.messages.forEach(message => {
-    if(message.type === "m.room.message") 
-    {
-        const mtype = message.content.msgtype;
-        if (mtype === "m.text") 
+    const getFileTime = (timestamp) => {
+        const ts = new Date(timestamp);
+        let hours = ts.getHours();
+        let ampm = "AM"
+        if(hours > 12)
         {
-            posts.push(messageToPost(message));
+            hours = hours - 12;
+            ampm = "PM"
         }
-        if (mtype === "m.image")
+        if(hours === 0)
         {
-            posts.push(iamgeToPost(message));
+            hours = 12;
         }
-        if (mtype === "m.file")
-        {
-            posts.push(fileToPost(message));
-        }
-        if (mtype === "m.file")
-        {
-            posts.push(videoToPost(message));
-        }
-    }
-});
+        const minutes = ts.getMinutes().toString().padStart(2, '0');
+        const seconds = ts.getSeconds().toString().padStart(2, '0');
 
-const allUsers = [];
+        const suffix = `-${ts.getMonth()+1}-${ts.getDate()}-${ts.getFullYear()} at ${hours}-${minutes}-${seconds} ${ampm}`
 
-for(const [key, value] of Object.entries(users)) {
-    const user = {
-        type: "user",
-        user: {
-            username: value,
-            email: value + "@localhost",
-            use_markdown_preview: "false",
-            use_formatting: "false",
-            show_unread_section: "false",
-            email_interval: "hour"
-        }
+        console.log(suffix);
+        return suffix;
     }
 
-    allUsers.push(user);
+    const injectFileTime = (filename, timestamp) => {
+        const filetime = getFileTime(timestamp);
+
+        const index = filename.lastIndexOf(".");
+        const filetype = filename.slice(index);
+        const name = filename.slice(0, index);
+
+        return name + filetime + filetype;
+    }
+
+    const messageToPost = (message) => {
+        const user = message.user_id;
+        if(!users[user])
+        {
+            users[user] = user.slice(1, user.length - 23) + "_matrix";
+        }
+
+        const post = {
+            type: "post",
+            post: {
+                team,
+                channel: room,
+                user: users[user],
+                message: message.content.body,
+                create_at: message.origin_server_ts
+            }
+        }
+
+        return post;
+    }
+
+    const iamgeToPost = (message) => {
+        const user = message.user_id;
+        if(!users[user])
+        {
+            users[user] = user.slice(1, user.length - 23) + "_matrix";
+        }
+
+        const filename = injectFileTime(message.content.body, message.origin_server_ts);
+
+        console.log(filename)
+        const post = {
+            type: "post",
+            post: {
+                team,
+                channel: room,
+                user: users[user],
+                message: message.content.body,
+                create_at: message.origin_server_ts,
+                attachments: [{
+                    path: "/tmp/import/images/" + filename
+                }]
+            }
+        }
+
+
+
+        return post;
+    }
+
+    const fileToPost = (message) => {
+        const user = message.user_id;
+        if(!users[user])
+        {
+            users[user] = user.slice(1, user.length - 23) + "_matrix";
+        }
+
+        const filename = injectFileTime(message.content.body, message.origin_server_ts);
+        console.log(filename)
+        const post = {
+            type: "post",
+            post: {
+                team,
+                channel: room,
+                user: users[user],
+                message: message.content.body,
+                create_at: message.origin_server_ts,
+                attachments: [{
+                    path: "/tmp/import/files/" + filename
+                }]
+            }
+        }
+
+        return post;
+    }
+
+    const videoToPost = (message) => {
+        const user = message.user_id;
+        if(!users[user])
+        {
+            users[user] = user.slice(1, user.length - 23) + "_matrix";
+        }
+
+        const filename = injectFileTime(message.content.body, message.origin_server_ts);
+        console.log(filename)
+        const post = {
+            type: "post",
+            post: {
+                team,
+                channel: room,
+                user: users[user],
+                message: message.content.body,
+                create_at: message.origin_server_ts,
+                attachments: [{
+                    path: "/tmp/import/videos/" + filename
+                }]
+            }
+        }
+
+        return post;
+    }
+
+
+
+    info.messages.forEach(message => {
+        if(message.type === "m.room.message") 
+        {
+            const mtype = message.content.msgtype;
+            if (mtype === "m.text") 
+            {
+                posts.push(messageToPost(message));
+            }
+            if (mtype === "m.image")
+            {
+                posts.push(iamgeToPost(message));
+            }
+            if (mtype === "m.file")
+            {
+                posts.push(fileToPost(message));
+            }
+            if (mtype === "m.file")
+            {
+                posts.push(videoToPost(message));
+            }
+        }
+    });
+
+    const allUsers = [];
+
+    for(const [key, value] of Object.entries(users)) {
+        const user = {
+            type: "user",
+            user: {
+                username: value,
+                email: value + "@localhost",
+                use_markdown_preview: "false",
+                use_formatting: "false",
+                show_unread_section: "false",
+                email_interval: "hour"
+            }
+        }
+
+        allUsers.push(user);
+    }
+
+
+    const version = JSON.stringify({
+        type: "version",
+        version: 1
+    });
+    const userOutput = allUsers.map(user => JSON.stringify(user)).join('\n');
+    const postOutput = posts.map(post => JSON.stringify(post)).join('\n');
+
+
+
+    fs.writeFile(`.\\output\\${room}.jsonl`, version + '\n' + channel + '\n' + userOutput + '\n' + postOutput, (err) => {
+        if(err) {
+            throw err;
+        }
+    });
+    
 }
 
-
-const version = JSON.stringify({
-    type: "version",
-    version: 1
-});
-const userOutput = allUsers.map(user => JSON.stringify(user)).join('\n');
-const postOutput = posts.map(post => JSON.stringify(post)).join('\n');
-
-
-
-fs.writeFile(`output-${room}.jsonl`, version + '\n' + channel + '\n' + userOutput + '\n' + postOutput, (err) => {
-    if(err) {
-        throw err;
-    }
-})
+start();
